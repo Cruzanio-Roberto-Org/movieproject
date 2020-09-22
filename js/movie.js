20da54de0d8315f108511919ce377d607cdcd5ac
robertocuyar <roberto.cuyar44@gmail.com>
2020-09-22 14:03:54 -0500

    basic structure set up for functionality v0.1

---
+++ js/movie.js
@@ -0,0 +1,28 @@
+
+/*TODO Movie displayed in cards
+*
+* TODO Advertised movies in a carousel
+*  TODO carousel advertises the top three rated movies, title of carousel connects user to seeing what is popular
+* TODO each card outer border with image metal plating
+*  image with the movie from api
+* TODO click on image reveals movie info/ maybe a hover effect?
+*  TODO splash screen when card not chosen/ popcorn guy
+*  TODO click on image will create a container with info on top of cards and carousel moves up
+*  TODO movie info consists of title, description, movie rating, ranking system, genre, actors,
+*   TODO ranking system
+*    TODO actors will be like two or three lead actors
+*     TODO filters for the movie infos prioritize movie rating
+*      TODO loading screen will be on splash
+*       TODO get request for page load
+*        TODO add a movie as a modal using forms
+*         TODO when user adds a movie do a get request for the movie's image from the api/ have a default image in case the movie doesn't have an image in database or in general
+*          TODO delete info button on bottom left that deletes from db
+*          TODO edit info in movies we need to add an edit anchor in middle
+*           TODO book movie button on the bottom right
+*            TODO disable attribute on click listeners when loading
+*             TODO drag and drop with a clip of film roll
+*              TODO trailers display option
+*
+* */
+
+
