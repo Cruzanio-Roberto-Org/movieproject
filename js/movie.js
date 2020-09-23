2eb01dfec212ac47f128e565a52f897b59aa80f6
robertocuyar <roberto.cuyar44@gmail.com>
2020-09-23 11:10:29 -0500

    adding a movie functionality and search by title feature v0.3

--- js/movie.js
+++ js/movie.js
@@ -3,6 +3,8 @@
 
         let movURL = 'https://ebony-palm-titanosaurus.glitch.me/movies'
 
+        const toggleLoad = () => $('.lds-grid').toggleClass('d-none')
+
         function classToggle() {
             $('#popcorn-guy').addClass('d-none')
             $('.cardTog').removeClass('d-none')
@@ -16,24 +18,54 @@
                         classToggle()
                         $('#title').html(title)
                         $('#rating').html(rating)
-                        $('#description').html(description)
+                        $('#info').html(description)
                         $('#genre').html(genre)
                         $('#actors').html(actors)
-
+                        $('.card-body').attr('data-serv', $(this).attr('id'))
                     })
             })
         }
 
-        fetch(movURL)
-            .then(res => res.json())
-            .then(data => {
-                for (let element of data) {
-                    $('#img-display').append(`<div class="dis-hover"><img id='${element.id}' src="img/theaterentrance.jpg"></div>`)
-                }
-                listener1()
-            })
-
-
+        const pageLoad = () => {
+            fetch(movURL)
+                .then(res => res.json())
+                .then(data => {
+                    toggleLoad();
+                    console.log(data)
+                    for (let element of data) {
+                        $('#img-display').append(`<div class="dis-hover"><img id='${element.id}' src="img/theaterentrance.jpg"></div>`)
+                    }
+                    listener1()
+                })
+        }
+        $('#filter-button').click(() => {
+            searchFilter()
+        })
+        const searchFilter = () => {
+            let input = $('#filter-search').val().toLowerCase();
+            $('#img-display').children().remove();
+            toggleLoad()
+            fetch(movURL)
+                .then(res => res.json())
+                .then(data => {
+                    let newData = data.filter(({searchables}) => searchables.includes(input))
+                    toggleLoad()
+                    for (let element of newData) {
+                        $('#img-display').append(`<div class="dis-hover"><img id='${element.id}' src="img/theaterentrance.jpg"></div>`)
+                    }
+                    listener1()
+                })
+        }
+        $('#update-data').click(()=>{
+            let newInfo = {title: $('#add-title').val(), rating: $('#add-rating').val(), description: $('#add-des').val()}
+            console.log($('#add-des').val())
+            fetch(movURL, {
+                method: 'POST',
+                headers: {"Content-Type" :"application/json"},
+                body: JSON.stringify(newInfo)
+            }).then(pageLoad)
+        })
+        pageLoad();
     })
 })(jQuery);
 /*Movie displayed in cards
@@ -41,15 +73,14 @@
 * TODO Advertised movies in a carousel
 *  TODO carousel advertises the top three rated movies, title of carousel connects user to seeing what is popular
 * each card outer border with image metal plating
-*  TODO image with the movie from api
-*  TODO splash screen when card not chosen/ popcorn guy
-*  TODO click on image will create a container with info on top of cards and carousel moves up
-*  TODO movie info consists of title, description, movie rating, ranking system, genre, actors,
-*   TODO ranking system
-*    TODO actors will be like two or three lead actors
-*     TODO filters for the movie infos prioritize movie rating
-*      TODO loading screen will be on splash
-*       TODO get request for page load
+*
+* splash screen when card not chosen/ popcorn guy
+*  click on image will create a container with info on top of cards and carousel moves up
+*   movie info consists of title, description, movie rating, ranking system, genre, actors,
+* actors will be like two or three lead actors
+*
+*loading screen will be on splash
+* get request for page load
 *        TODO add a movie as a modal using forms
 *         TODO when user adds a movie do a get request for the movie's image from the api/ have a default image in case the movie doesn't have an image in database or in general
 *          TODO delete info button on bottom left that deletes from db
@@ -58,6 +89,9 @@
 *            TODO disable attribute on click listeners when loading
 *             TODO drag and drop with a clip of film roll
 *              TODO trailers display option
+*               TODO image with the movie from api
+*                TODO ranking system
+*                  TODO filters for the movie infos prioritize movie rating
 *TODO reveals movie info/ maybe a hover effect?
 * */
 
